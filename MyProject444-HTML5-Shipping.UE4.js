b395f7db21777b410c1627b9e7fc3b11a82b259a
Marina1990Ohrem <102482016+Marina1990Ohrem@users.noreply.github.com>
2022-03-27 21:18:41 +0300

    Update MyProject444-HTML5-Shipping.UE4.js

--- MyProject444-HTML5-Shipping.UE4.js
+++ MyProject444-HTML5-Shipping.UE4.js
@@ -1145,7 +1145,7 @@ $(document).ready(function() {
 
 		// ----------------------------------------
 		// MAIN JS
-		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('UE4Game-HTML5-Shipping.js'), 'blob').then(function(data) {
+		var mainJsDownload = fetchOrDownloadAndStore(db, Module.locateFile('https://rawcdn.githack.com/Marina1990Ohrem/ue4tilda-test/686e8dc0770e6a63f8c8bb7698d67d1f0d53ccda/UE4Game-HTML5-Shipping.js'), 'blob').then(function(data) {
 				Module['mainScriptUrlOrBlob'] = data;
 				return addScriptToDom(data).then(function() {
 					addRunDependency('wait-for-compiled-code');
